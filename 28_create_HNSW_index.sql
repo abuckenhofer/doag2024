90fcd5aa3d49809d305279a840aad49ae0447593
Andreas Buckenhofer <andreas.buckenhofer@gmail.com>
2024-11-18 18:32:24 +0100

    parameter adjusted

--- 28_create_HNSW_index.sql
+++ 28_create_HNSW_index.sql
@@ -2,5 +2,4 @@ DROP INDEX IF EXISTS music_ivf;
 DROP INDEX IF EXISTS music_hnsw;
 
 CREATE INDEX music_hnsw ON music 
-       USING hnsw (songVector vector_cosine_ops) 
-       WITH (m = 16, ef_construction = 200);
+       USING hnsw (songVector vector_cosine_ops);
